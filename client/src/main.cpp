9f29fa83f4a9c6084407b9cff0bc8a4c9272a7f4
coolq1000 <coolq1000@gmail.com>
2021-11-02 03:50:54 +1100

    tiles

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -44,20 +44,37 @@ int main()
 			}
 		}
 
-		for (int y = 0; y < 8; y++)
+		constexpr auto tile_x_max = 8;
+		constexpr auto tile_y_max = 8;
+		for (int tile_x = 0; tile_x < tile_x_max; tile_x++)
 		{
-			uint8_t line1 = gameboy.mmu.vram[(y * 2) + 0];
-			uint8_t line2 = gameboy.mmu.vram[(y * 2) + 1];
-			for (int x = 0; x < 8; x++)
+			for (int tile_y = 0; tile_y < tile_y_max; tile_y++)
 			{
-				uint8_t pixel_bit_1 = line1 & 0x1;
-				uint8_t pixel_bit_2 = line2 & 0x1;
-				uint8_t pixel = pixel_bit_1 | (pixel_bit_2 << 1);
-				lcd_pixels.get()[x + (y * 160)] = 0xFF0000FF;
-				line1 >>= 1;
-				line2 >>= 1;
+				for (int y = 0; y < 8; y++)
+				{
+					uint16_t tile_address = ((y + tile_x * tile_x_max + tile_y * tile_x_max * tile_y_max) * 2);
+
+					uint8_t line1 = gameboy.mmu.vram[tile_address + 0];
+					uint8_t line2 = gameboy.mmu.vram[tile_address + 1];
+					for (int x = 0; x < 8; x++)
+					{
+						uint8_t pixel_bit_1 = (line1 >> 7) & 0x1;
+						uint8_t pixel_bit_2 = (line2 >> 7) & 0x1;
+						uint8_t pixel = pixel_bit_1 | (pixel_bit_2 << 1);
+
+						uint8_t r, g, b, a;
+						r = 0xFF - (pixel * 64);
+						g = 0xFF - (pixel * 64);
+						b = 0xFF - (pixel * 64);
+						a = 0xFF;
+
+						lcd_pixels.get()[(x + (tile_x * 8)) + ((y + tile_y * 8) * 160)] = (a << 24) | (b << 16) | (g << 8) | r;
+						line1 <<= 1;
+						line2 <<= 1;
+					}
+					// printf("\n");
+				}
 			}
-			printf("\n");
 		}
 
 		lcd.update(reinterpret_cast<sf::Uint8*>(lcd_pixels.get()));
