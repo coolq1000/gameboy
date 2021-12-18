45b2b13e998bb3b1cb49b9008ab40f158972fe7e
taigaclaw <coolq1000@gmail.com>
2021-12-18 23:46:23 +1100

    commit audio files

---
+++ client/include/audio.hpp
@@ -0,0 +1,28 @@
+
+#ifndef AUDIO_HPP
+#define AUDIO_HPP
+
+#include <SFML/Audio.hpp>
+#include "core/util.h"
+#include "core/dmg.hpp"
+
+constexpr usize sample_rate = 44100;
+
+class audio_stream : public sf::SoundStream
+{
+    usize current_sample;
+    std::vector<sf::Int16> samples;
+
+    gmb::apu apu;
+
+    virtual bool onGetData(Chunk& data);
+    virtual void onSeek(sf::Time timeOffset);
+
+public:
+
+    audio_stream(gmb::apu apu);
+
+    void load();
+};
+
+#endif
---
+++ client/src/audio.cpp
@@ -0,0 +1,36 @@
+#include "audio.hpp"
+
+bool audio_stream::onGetData(Chunk& data)
+{
+    /* emit samples into buffer */
+    for (usize seek = 0; seek < sample_rate; seek++)
+    {
+        samples[seek] = apu.emit(seek, getSampleRate()) * 0.05f;
+    }
+
+    data.samples = &samples[current_sample];
+    data.sampleCount = sample_rate;
+    return true;
+}
+
+void audio_stream::onSeek(sf::Time timeOffset)
+{
+    current_sample = static_cast<usize>(timeOffset.asSeconds() * getSampleRate() * getChannelCount());
+}
+
+audio_stream::audio_stream(gmb::apu apu) : apu(apu)
+{
+    load();
+}
+
+void audio_stream::load()
+{
+    /* fill with empty samples */
+    samples.resize(sample_rate);
+
+    /* reset current sample to beginning */
+    current_sample = 0;
+
+    /* call back to the super (sf::SoundStream) for initialization */
+    initialize(1, sample_rate);
+}
